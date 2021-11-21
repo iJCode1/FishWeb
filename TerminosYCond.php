f47b36f822d9b4f69d344d20a15e839e06b43c8f
Fernando Antonio <fantoniogonzalez1999@gmail.com>
2021-11-21 17:56:51 -0600

    creacion de login, terminos y cond y agrege librerias

---
+++ TerminosYCond.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Terminos y condiciónes</title>
+    <link rel="stylesheet" href="librerias/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="terminos">
+                    <h1>Terminos y condiciónes</h1>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime 
+                        temporibus provident odio possimus adipisci aut illum dolorum quam 
+                        assumenda eos harum vero, ad exercitationem vitae repellat autem 
+                        laudantium sed id!</p>
+
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime 
+                        temporibus provident odio possimus adipisci aut illum dolorum quam 
+                        assumenda eos harum vero, ad exercitationem vitae repellat autem 
+                        laudantium sed id!</p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime 
+                        temporibus provident odio possimus adipisci aut illum dolorum quam 
+                        assumenda eos harum vero, ad exercitationem vitae repellat autem 
+                        laudantium sed id!</p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime 
+                        temporibus provident odio possimus adipisci aut illum dolorum quam 
+                        assumenda eos harum vero, ad exercitationem vitae repellat autem 
+                        laudantium sed id!</p>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime 
+                        temporibus provident odio possimus adipisci aut illum dolorum quam 
+                        assumenda eos harum vero, ad exercitationem vitae repellat autem 
+                        laudantium sed id!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="librerias/jquery.min.js"></script>
+    <script src="librerias/popper.min.js"></script>
+    <script src="librerias/bootstrap.min.js"></script>
+</body>
+</html>
--- datosPersonalesEscritor.php
+++ datosPersonalesEscritor.php
@@ -4,9 +4,70 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Registro Escritor</title>
+    <link rel="stylesheet" href="librerias/bootstrap.min.css">
+
 </head>
 <body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="registrarse">
+                    <div class="text-center">
+                        <h1>Registro de escritor</h1>
+                    </div>
+                    
+                    <form>
+                        <div class="form-group">
+                            <label for="nombre">Nombre</label>
+                            <input required type="text" class="form-control" id="nombre" placeholder="Nombre">                            
+                        </div>
+                        <div class="form-group">
+                            <label for="apPat">Apellido Paterno</label>
+                            <input required type="text" class="form-control" id="apPat" placeholder="Apellido Paterno">
+                        </div>
+                        <div class="form-group">
+                            <label for="apMat">Apellido Materno</label>
+                            <input required type="text" class="form-control" id="apMat" placeholder="Apellido Materno">
+                        </div>
+                        <div class="form-group">
+                            <label for="RFC">RFC</label>
+                            <input required type="text" class="form-control" id="RFC" placeholder="RFC">
+                        </div>
+                        <div class="form-group">
+                            <label for="Edad">Edad</label>
+                            <input required type="text" class="form-control" id="Edad" placeholder="Edad">
+                        </div>
+                        <div class="form-group">
+                            <label for="Direccion">Dirección</label>
+                            <input required type="text" class="form-control" id="Direccion" placeholder="Dirección">
+                        </div>
+                        <div class="form-group">
+                            <label for="Telefono">Teléfono</label>
+                            <input required type="text" class="form-control" id="Telefono" placeholder="Teléfono">
+                        </div>
+                        <div class="form-group">
+                            <label for="RefArt">Referencias de articulos</label>
+                            <input required type="text" class="form-control" id="RefArt" placeholder="Referencias de articulos">
+                        </div>
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="terminos" required>
+                            <label class="form-check-label" for="terminos">Marca la casilla si estas de acuerdo con nuestros <a target="_blank" href="TerminosYCond.php">Terminos y condiciónes</a></label>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary mt-3">Submit</button>
+                        </div>
+                        
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
     
+
+    <script src="librerias/jquery.min.js"></script>
+    <script src="librerias/popper.min.js"></script>
+    <script src="librerias/bootstrap.min.js"></script>
+    <script src="scripts/validar.js"></script>
 </body>
 </html>
