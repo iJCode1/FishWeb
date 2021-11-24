1bcfd94efae85a6f582530232e1d3b1214b0ee6a
CindyMon <cindymonroy@hotmail.es>
2021-11-24 16:34:11 -0600

    readme

--- FishWeb/Cindy/modificar.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-<p>
-Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur, quas nemo in libero odit enim maiores soluta inventore nam? Nostrum alias ab commodi quas pariatur enim quam tempora recusandae iure!
-</p>
-
-?>
---
+++ README.md
@@ -0,0 +1,3 @@
+# FishWeb
+Proyecto de la materia de programación web
+Sitio de articulos relacionados con la pesca o Acuicultura
