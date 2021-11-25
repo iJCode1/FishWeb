69df6df0b28fc930af31b5be5056a824543f33c5
CindyMon <cindymonroy@hotmail.es>
2021-11-24 19:35:48 -0600

    conexion a la bd

---
+++ FishWeb/Cindy/db.php
@@ -0,0 +1,19 @@
+<?php
+
+$servidor = "localhost";
+$usuarioBD = "root";
+$pwdBD = "";
+$nomBD = "fishWeb";
+
+$db = mysqli_connect($servidor, $usuarioBD, $pwdBD, $nomBD);
+
+if (!$db) 
+{
+    die("La conexión fallo: " . mysqli_connect_error());
+} 
+else 
+{
+    mysqli_query($db, "SET NAMES 'UTF8'");
+    echo 'Conexión exitosa <br>';
+}
+?>
