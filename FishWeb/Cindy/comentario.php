f224ea15a9f9d401828359b939575dbdff671398
CindyMon <cindymonroy@hotmail.es>
2021-11-29 05:41:07 -0600

    registrar comentario

--- FishWeb/Cindy/comentario.php
+++ FishWeb/Cindy/comentario.php
@@ -9,7 +9,7 @@ echo $articulo;
 echo $comentario;
 
 
-$sentencia3 = "INSERT INTO comentarios(usuario,articulo,comentario) VALUES('$usuario','$articulo','$comentario')";
+$sentencia2 = "INSERT INTO comentarios(usuario,articulo,comentario) VALUES('$usuario','$articulo','$comentario')";
 if (mysqli_query($db, $sentencia3)) 
 {
 header('Location: articulo.php?mensaje=registrado');
